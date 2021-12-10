358281e5a1c63835792c2f3bc8e0300cdaed127d
Chu Minh Dao - Loser Chu <chudao63@gmail.com>
2021-12-10 16:10:29 +0700

    update

--- app/fms/map/apis.py
+++ app/fms/map/apis.py
@@ -2,10 +2,12 @@ import re
 from sqlalchemy.sql.sqltypes import REAL
 from app.models.map import Maps
 from utils.apimodel import BaseApiPagination
-from flask_restful import Resource, reqparse,request
-from app.models.groups import 
+from flask_restful import Resource, reqparse, request
 import os, sys
 from flask import send_from_directory
+from flask import Flask
+from app import app
+
 
 class MapsApi(BaseApiPagination):
     """
@@ -13,8 +15,3 @@ class MapsApi(BaseApiPagination):
     """
     def __init__(self):
         BaseApiPagination.__init__(self, Maps, "/map")
-
-
-class UploadFileApi(Resource):
-    def post(self):
-        pass
