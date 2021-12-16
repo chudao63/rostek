32a6168d87eca0a8ce77c697c6ffd7121345050c
Chu Minh Dao - Loser Chu <chudao63@gmail.com>
2021-12-16 11:55:32 +0700

    update send waypoit, command to agv ok

--- app/ros/subcriber.py
+++ app/ros/subcriber.py
@@ -41,29 +41,35 @@ def handle_mqtt_message(client, userdata, message):
 		topic=message.topic
 		payload=json.loads(message.payload.decode())
 		logging.info(f"payload--> {payload}")
-		robot_id = payload["robot_id"]
-		cmd = payload["cmd"]
 
-		if cmd == 'load':
-			print("load")
-			position = {'y': 4, 'x':5, 'z': 7}
-			orientation ={'y': 8, 'x': 11, 'z': 12, 'w': 4}
-			Monitor.getInstance().robots[robot_id].send_message_to_agv(3,1,1,position,orientation) #gửi lệnh xuống agv, có 3 tham số, use poit_type
-			# Monitor.getInstance().robots[robot_id].finish_load() #gửi lệnh xuống agv, có 3 tham số, use poit_type
+		robot_id = payload.get("robot_id")
+		cmd = payload.get("cmd")
+		point_type = payload.get("point_type")
+		position	= payload.get('position')
+		orientation = payload.get('orientation')
+		
+		if cmd and robot_id:
+			if cmd == 'load':
+				print("load")
+				# position = {'y': 14, 'x':25, 'z': 37}
+				# orientation ={'y': 81, 'x': 121, 'z': 172, 'w': 54}
+				# Monitor.getInstance().robots[robot_id].send_message_to_agv(1,1,1,position,orientation) #gửi lệnh xuống agv, có 3 tham số, use poit_type
+				Monitor.getInstance().robots[robot_id].finish_load() 
+				
+			if cmd == 'unload':
+				print("unload")
+				Monitor.getInstance().robots[robot_id].finish_unload() 
+
+		if point_type and position and orientation:
+			Monitor.getInstance().robots[robot_id].send_message_to_agv(point_type,1,1,position,orientation) 
+
 			
-		if cmd == 'unload':
-			print("unload")
-			position = {'y': 2, 'x':3, 'z': 0}
-			orientation ={'y': 12, 'x': 3, 'z': 5, 'w': 1}
-			Monitor.getInstance().robots[robot_id].send_message_to_agv(4,1,1,position,orientation) #gửi lệnh xuống agv, có 3 tham số, use poit_typequ
-	
-		
-		if cmd == 'moving':
-			print("moving")
-			position = {'y': 14, 'x':25, 'z': 37}
-			orientation ={'y': 81, 'x': 121, 'z': 172, 'w': 54}
-			Monitor.getInstance().robots[robot_id].send_message_to_agv(1,1,1,position,orientation) #gửi lệnh xuống agv, có 3 tham số, use poit_type
-		
+# if cmd == 'moving':
+# 	print("moving")
+# 	position = {'y': 14, 'x':25, 'z': 37}
+# 	orientation ={'y': 81, 'x': 121, 'z': 172, 'w': 54}
+# 	Monitor.getInstance().robots[robot_id].send_message_to_agv(1,1,1,position,orientation) #gửi lệnh xuống agv, có 3 tham số, use poit_type
+
 	
 
 			
