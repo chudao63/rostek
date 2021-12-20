82b9fbafdda102183b9eb4ef8a8d7f309b197565
Chu Minh Dao - Loser Chu <chudao63@gmail.com>
2021-12-20 14:19:46 +0700

    Thêm active ở bảng Group

--- app/fms/groups/apis.py
+++ app/fms/groups/apis.py
@@ -1,8 +1,10 @@
 import re
+from sqlalchemy.sql.functions import ReturnTypeFromArgs
 from sqlalchemy.sql.sqltypes import REAL
 from utils.apimodel import BaseApiPagination
 from flask_restful import Resource, reqparse,request
 from app.models.group import Group
+from app import db
 
 class GroupApi(BaseApiPagination):
     """
@@ -10,3 +12,11 @@ class GroupApi(BaseApiPagination):
     """
     def __init__(self):
         BaseApiPagination.__init__(self, Group, "/group")
+class DeactiveGroupApi(Resource):
+    def patch(self):
+        data = request.get_json(force = True)
+        group = Group.query.get(data["id"])
+        group.active = data["active"]
+        db.session.add(group)
+        db.session.commit()
+        return "patch done!"
--- app/models/group.py
+++ app/models/group.py
@@ -10,6 +10,7 @@ class Group(db.Model, DbBaseModel):
     __tablename__ = 'group'
     id          = Column(Integer, primary_key= True, autoincrement= True, nullable= False)
     name        = Column(String(50), unique= True, nullable= False)
+    active      = Column(String(50),default= True ,nullable= True)
     # mission_id  = Column(Integer, ForeignKey('mission.id'), nullable= False)
 
 
