0d0527ab3f567d4cf2615b373f4e5f5432bd1921
Chu Minh Dao - Loser Chu <chudao63@gmail.com>
2021-12-29 15:13:58 +0700

    Sửa code trong phần PATCH MISSION thành hàm

--- app/fms/mission/apis.py
+++ app/fms/mission/apis.py
@@ -20,9 +20,65 @@ class MissionBaseApi(BaseApiPagination):
         BaseApiPagination.__init__(self, Mission, "/mission-base")
 
 
+
+def edit_step_data(data):
+    """
+    Sửa các step có trong 1 Mission
+    data: dữ liệu json truyền vào
+    """
+    for stepIndex in data['steps']:
+        if "id" in stepIndex:
+            startPointId = stepIndex['start_point_id']
+            startPointDb = Position.query.get(startPointId)
+
+            assert startPointDb is not None, f"Point {startPointId} không tồn tại"
+            endPointId = stepIndex['end_point_id']
+            endPointDb = Position.query.get(endPointId)
+
+            assert endPointDb is not None, f"Point {endPointId} không tồn tại"
+            step = Step.query.get(stepIndex['id'])
+            assert step is not None, f"Step {stepIndex['id']} không tồn tại"
+
+            step.start_point = stepIndex['start_point_id']
+            step.end_point = stepIndex['end_point_id']
+            while len(step.products): # xoa het cac product trong step
+                step.products.pop(0)
+            # them moi product
+
+            productId = stepIndex["product_id"]
+            productDb = Product.query.get(productId)
+            assert productDb is not None, f"Product {productId} không tồn tại"
+
+            step.products.append(productDb)
+            db.session.add(step)
+            db.session.commit()
+
+def new_step_data(missionDb, start_point, end_point, productData):
+    """
+    Ghi thêm một step mới vào Mission cũ
+    missionDb  : Mission muốn sửa
+    start_point: Điểm bắt đầu của step
+    end_point  : Điểm kết thúc của step
+    productData: Hàng hóa phục vụ cho step
+    """
+    step = Step(start_point = start_point, end_point = end_point)
+    db.session.add(step)
+    db.session.commit()
+    stepMission = Step.query.order_by(Step.id.desc()).first()
+    product = Product.query.get(productData)
+    stepMission.products.append(product)
+
+    missionDb.steps.append(stepMission)
+    db.session.add(missionDb)
+    db.session.commit()
+
+
+
 class MissionApi(Resource):
     def get(self):
-       
+        """
+        Lấy dữ liệu các Mission có trong database
+        """
         missions = Mission.query.all()
         output = []
         for mission in missions:
@@ -145,44 +201,8 @@ class MissionApi(Resource):
             if dataIndex == 'steps':                
                 for stepIndex in data['steps']:
                     if "id" in stepIndex:
-                        # edit_step_data()
-                        startPointId = stepIndex['start_point_id']
-                        startPointDb = Position.query.get(startPointId)
-
-                        assert startPointDb is not None, f"Point {startPointId} không tồn tại"
-                        endPointId = stepIndex['end_point_id']
-                        endPointDb = Position.query.get(endPointId)
-
-                        assert endPointDb is not None, f"Point {endPointId} không tồn tại"
-                        step = Step.query.get(stepIndex['id'])
-                        assert step is not None, f"Step {stepIndex['id']} không tồn tại"
-
-                        step.start_point = stepIndex['start_point_id']
-                        step.end_point = stepIndex['end_point_id']
-                        while len(step.products): # xoa het cac product trong step
-                            step.products.pop(0)
-                        # them moi product
-
-                        productId = stepIndex["product_id"]
-                        productDb = Product.query.get(productId)
-                        assert productDb is not None, f"Product {productId} không tồn tại"
-
-                        step.products.append(productDb)
-                        db.session.add(step)
-                        db.session.commit()
+                        edit_step_data(data)
                     if "id" not in stepIndex:
-                        step = Step(start_point = stepIndex['start_point_id'], end_point = stepIndex['end_point_id'])
-                        db.session.add(step)
-                        db.session.commit()
-                        stepMission = Step.query.order_by(Step.id.desc()).first()
-                        product = Product.query.get(stepIndex['product_id'])
-                        stepMission.products.append(product)
-
-                        mission.steps.append(stepMission)
-                        db.session.add(mission)
-                        db.session.commit()
-
-                        
-                        
+                        new_step_data(mission, stepIndex['start_point_id'],stepIndex['end_point_id'],stepIndex['product_id'])
         return create_response_message("Sửa thành công", 200)
  
