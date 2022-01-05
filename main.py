98ba65739e9ff6f91e57798582454d46bebc8227
@nguyen.khac.cong.1998 <nguyen.khac.cong.1998@gmail.com>
2022-01-05 17:16:19 +0700

    bat socketio

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from app.users.urls import *
 from app.update.urls import *
 from app.fms.urls import *
 # from app.ros.subcriber import *
-
+from app import socketio
 
 
 
 
 if __name__ == "__main__":
-    # if MqttConfigure.ACTIVE:
-    #     socketio.run(app, host=FlaskConfigure.HOST, port=FlaskConfigure.PORT, use_reloader=True , debug=FlaskConfigure.DEBUG)
-    # else:
-    #     socketio.run(app, host=FlaskConfigure.HOST, port=FlaskConfigure.PORT, use_reloader=True , debug=FlaskConfigure.DEBUG)
-    app.run(host=FlaskConfigure.HOST, port=FlaskConfigure.PORT, use_reloader=False , debug=False)
+    if MqttConfigure.ACTIVE:
+        socketio.run(app, host=FlaskConfigure.HOST, port=FlaskConfigure.PORT, use_reloader=True , debug=FlaskConfigure.DEBUG)
+    else:
+        socketio.run(app, host=FlaskConfigure.HOST, port=FlaskConfigure.PORT, use_reloader=True , debug=FlaskConfigure.DEBUG)
+    # app.run(host=FlaskConfigure.HOST, port=FlaskConfigure.PORT, use_reloader=False , debug=False)
